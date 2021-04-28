82273ca3219d790af5d9744fd1578d87e04ec576
pawan singh <kushpawal1@gmail.com>
2021-04-28 17:37:47 +0530

    [user2] added code for generate random number to roll dice

--- Play.java
+++ Play.java
@@ -3,7 +3,13 @@ class Play
 	public static void main(String [] args)
 	{
 		System.out.println("Welcome to Snake and ladder game:");
+		//user 1 added position varible
 		int position=0;
+
+		//user 2 added Random function to roll the dice
+		Random r=new Random();
+		int num=r.nextInt(5);
+		num=num+1;
 
 
 	}
