7eb06b70c46877fdf39d74a075113fe6ed7a1ecb
pawan singh <kushpawal1@gmail.com>
2021-04-28 17:49:18 +0530

    [user3] added code for options.

--- Play.java
+++ Play.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Play
 {
 	public static void main(String [] args)
@@ -8,8 +9,26 @@ class Play
 
 		//user 2 added Random function to roll the dice
 		Random r=new Random();
-		int num=r.nextInt(5);
-		num=num+1;
+		int dice=r.nextInt(6);
+		dice=dice+1;
+
+		//user 3 added code for the option .
+		int option=r.nextInt(3);
+
+		switch(option) {
+
+		case 0:
+			System.out.println("No play");
+		break;
+
+		case 1:
+			System.out.println("Snack");
+		break;
+
+		case 2:
+			System.out.println("Ladder");
+		break;
+		}
 
 
 	}
